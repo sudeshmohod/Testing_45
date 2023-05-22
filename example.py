59bf8287b065f1a8b0857244f424ac432eba9711
sudeshmohod <sudeshmohod12@gmail.com>
2023-05-22 18:57:53 +0530

    this was another code

--- example.py
+++ example.py
@@ -1,4 +1,21 @@
-a=100
-b=30
-c=a-b
-print(c)
+class Person:
+    '''this simple code i used'''
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+p = Person('John', 25)
+print(p.name,p.age)
+print(p.__doc__)
+class Person:
+    '''this other code used'''
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+if __name__ == '__main__':
+    person = Person('John', 25)
+    print(f"I'm {person.name}. I'm {person.age} years old.")
+print(person.__doc__)
