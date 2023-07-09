086722e67300dd4e57d51cf34a904f14c5cc5ab1
sudeshmohod <sudeshmohod12@gmail.com>
2023-07-09 09:45:58 +0530

    this is first commit for practice purpose

--- example.py
+++ example.py
@@ -1,15 +1,10 @@
-class Test:
-    x=10
-    def __init__(self):
-        self.y=20
-        self.z=30
-t1=Test()
-t2=Test()
-print('t1:',t1.x,t1.y,t1.z)
-print('t2:',t2.x,t2.y,t2.z)
-t1.x=888
-del Test.x
-t1.y=999
-print('t1:',t1.x,t1.y,t1.z)
-print('t2:',t2.x,t2.y)
-
+import psycopg2 as pg
+conn=pg.connect(user='postgres',
+                password='sudesh',
+                host='127.0.0.1',
+                port='5432')
+conn.autocommit=True
+cursor1=conn.cursor()
+sql='''create database "testing-45" '''
+cursor1.execute(sql)
+print('database created')
